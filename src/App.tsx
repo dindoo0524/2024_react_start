601dd0fe1f3c3bee08b0abf29c63015ea0dd4d29
JinjuNa <jinju@liveklass.com>
2024-03-17 16:03:13 +0900

    Reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,5 @@
-import React, { useState } from "react";
-
-interface Todo {
-  text: string;
-}
-
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTodo(event.target?.value);
-  };
-  const onButtonClick = () => {
-    if (todo === "") {
-      return;
-    }
-    setTodos((current) => [
-      ...current,
-      {
-        text: todo,
-      },
-    ]);
-    setTodo("");
-  };
-  return (
-    <div>
-      <input type="text" value={todo} onChange={onInputChange} />
-      <button onClick={onButtonClick}>Add</button>
-      <ul>
-        {todos.map((item, index) => (
-          <li key={index}> {item.text} </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
