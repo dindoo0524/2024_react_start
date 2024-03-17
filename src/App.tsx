95d4ef82159122ed577b71f729c6ee4d138198e0
JinjuNa <jinju@liveklass.com>
2024-03-17 16:25:45 +0900

    Coin Tracker (myself)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+
+interface Coin {
+  id: string;
+  name: string;
+  rank: number;
+  total_supply: number;
+}
 function App() {
-  return <div></div>;
+  const [coins, setConins] = useState<Coin[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetchCoins()
+      .then((data) => setConins(data))
+      .then(() => setLoading(false));
+  }, []);
+
+  async function fetchCoins(): Promise<Coin[]> {
+    const response = await fetch("https://api.coinpaprika.com/v1/tickers");
+    const data = await response.json();
+    return data;
+  }
+
+  return (
+    <div>
+      {loading ? (
+        <h3>loading....</h3>
+      ) : (
+        <div>
+          <ul>
+            {coins.map((coin, index) => (
+              <li key={index}>
+                {coin.id} {coin.name} {coin.rank}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
