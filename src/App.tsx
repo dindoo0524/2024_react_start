0899b3d3918b5c2322a255bc1b982785d4f3487c
JinjuNa <jinju@liveklass.com>
2024-03-17 16:26:42 +0900

    Reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,5 @@
-import { useEffect, useState } from "react";
-
-interface Coin {
-  id: string;
-  name: string;
-  rank: number;
-  total_supply: number;
-}
 function App() {
-  const [coins, setConins] = useState<Coin[]>([]);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    fetchCoins()
-      .then((data) => setConins(data))
-      .then(() => setLoading(false));
-  }, []);
-
-  async function fetchCoins(): Promise<Coin[]> {
-    const response = await fetch("https://api.coinpaprika.com/v1/tickers");
-    const data = await response.json();
-    return data;
-  }
-
-  return (
-    <div>
-      {loading ? (
-        <h3>loading....</h3>
-      ) : (
-        <div>
-          <ul>
-            {coins.map((coin, index) => (
-              <li key={index}>
-                {coin.id} {coin.name} {coin.rank}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
